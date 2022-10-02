48b4c14f602dfd7388f297c88ea6e6b0c4fb9a34
yeoojin <only1year100@gmail.com>
2022-10-02 21:06:21 +0900

    fix(Message): 게임 결과값 메세지 space 추가 refactor(Message): 중복코드 제거

--- src/main/java/baseball/model/Message.java
+++ src/main/java/baseball/model/Message.java
@@ -4,10 +4,13 @@ import baseball.constant.CommonConstant;
 
 public class Message {
 
+    private String blankString = CommonConstant.NULL;
+
     public Message (Baseball baseball) {
         StringBuilder message = new StringBuilder();
 
         message.append(appendBallResult(baseball));
+        message.append(appendSpace(baseball));
         message.append(appendStrikeResult(baseball));
         message.append(appendNothing(baseball));
     }
@@ -16,21 +19,28 @@ public class Message {
         if(baseball.getBallCnt() > 0) {
             return baseball.getBallCnt() + CommonConstant.BALL;
         }
-        return CommonConstant.NULL;
+        return blankString;
     }
 
     private String appendStrikeResult(Baseball baseball) {
         if(baseball.getStrikeCnt() > 0) {
             return baseball.getStrikeCnt() + CommonConstant.STRIKE;
         }
-        return CommonConstant.NULL;
+        return blankString;
+    }
+
+    private String appendSpace(Baseball baseball) {
+        if(baseball.getBallCnt() > 0 && baseball.getStrikeCnt() > 0) {
+            return CommonConstant.SPACE;
+        }
+        return blankString;
     }
 
     private String appendNothing(Baseball baseball) {
         if(baseball.isNothing()) {
             return CommonConstant.NOTHING;
         }
-        return CommonConstant.NULL;
+        return blankString;
     }
 
 
