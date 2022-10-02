fc27dd1f5f1533480cc6bc1dd191d79e1b39fab1
yeoojin <only1year100@gmail.com>
2022-10-02 22:56:56 +0900

    fix(README.md): README 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,6 +6,5 @@ public class Application {
     public static void main(String[] args) {
         GameController gameController = new GameController();
         gameController.gameSetting();
-
     }
 }
